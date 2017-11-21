7ac4479793dd21e891d2577c96b5b514c71769e4
charlenelecouturier <charlene.lecoutuier@gmail.com>
2017-11-21 18:44:24 +0100

    methode direCarte() faite pour le joueur physique

--- ProjetLO02/src/pckg/JoueurPhysique.java
+++ ProjetLO02/src/pckg/JoueurPhysique.java
@@ -2,6 +2,8 @@ package pckg;
 import java.util.Scanner;
 import java.util.Timer;
 import java.util.TimerTask;
+import java.lang.Object;
+
 
 public class JoueurPhysique extends Joueur {
 	Scanner sc = new Scanner(System.in);
@@ -37,21 +39,27 @@ public class JoueurPhysique extends Joueur {
 	@Override
 	public void DireCarte() {
 		// TODO Auto-generated method stub
-		System.out.println("Vite : dire 'CARTE'");
 		
-		long temps = 5000;                      // délai avant de répéter la tache : 5000 = 5 seconde
-        long startTime = 0;                    // délai avant la mise en route (0 demarre immediatement)
-        Timer timer = new Timer();             // création du timer
-        TimerTask tache = new TimerTask() {     // création et spécification de la tache à effectuer
-            @Override
-                public void run() {
-                       String reponse = sc.nextLine();// ici se qui doit être effectué
+		
+		
+		System.out.println("Vite ! Dites'CARTE' :");
+		long t = System.currentTimeMillis();// date actuelle en millisecondes
+        String reponse = sc.nextLine();
+		long t2 = System.currentTimeMillis();
+		if (t2-t>3800 || (!reponse.equals("carte") && !reponse.equals("Carte") && !reponse.equals("CARTE")))
+		{
+			System.out.println("Vous n'avez pas dit 'CARTE' à temps ! CONTRE-CARTE !");
+			this.piocher(1);
+		}
+		else { 
+			System.out.println("Vous dites 'CARTE'! ");
+		
                 }
-        };
-        timer.scheduleAtFixedRate(tache,startTime,temps);
-		
-		
+        
+
 	}
+		
+
 
 	@Override
 	public void DireContreCarte() {
