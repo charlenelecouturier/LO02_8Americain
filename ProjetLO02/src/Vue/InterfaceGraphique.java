e347de42aedc256fa9bb2819df6ef620f2688de9
charlenelecouturier <charlene.lecoutuier@gmail.com>
2017-12-20 16:51:12 +0100

    interface

---
+++ ProjetLO02/src/Vue/InterfaceGraphique.java
@@ -0,0 +1,82 @@
+package Vue;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import javax.swing.JButton;
+import java.awt.FlowLayout;
+import javax.swing.JLabel;
+
+public class InterfaceGraphique {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					InterfaceGraphique window = new InterfaceGraphique();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public InterfaceGraphique() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JPanel panel = new JPanel();
+		frame.getContentPane().add(panel, BorderLayout.WEST);
+		panel.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		
+		JButton btnCarte = new JButton("Carte");
+		panel.add(btnCarte);
+		
+		JButton btnContreCarte = new JButton("Contre Carte");
+		panel.add(btnContreCarte);
+		
+		JPanel panel_1 = new JPanel();
+		frame.getContentPane().add(panel_1, BorderLayout.NORTH);
+		
+		JPanel panel_2 = new JPanel();
+		frame.getContentPane().add(panel_2, BorderLayout.EAST);
+		
+		JLabel lblScore = new JLabel("Score :");
+		panel_2.add(lblScore);
+		
+		JPanel panel_3 = new JPanel();
+		frame.getContentPane().add(panel_3, BorderLayout.SOUTH);
+		
+		JLabel lblMain = new JLabel("Main");
+		panel_3.add(lblMain);
+		
+		JPanel panel_4 = new JPanel();
+		frame.getContentPane().add(panel_4, BorderLayout.CENTER);
+		
+		JLabel lblPioche = new JLabel("Pioche");
+		panel_4.add(lblPioche);
+		
+		JLabel lblTalon = new JLabel("Talon");
+		panel_4.add(lblTalon);
+	}
+
+}
